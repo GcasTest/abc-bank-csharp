deafb4dcd1577844b84bf993fbc2da83256c4d9d
applicant <yanyan.m.wang@gmail.com>
2016-01-31 22:45:47 -0500

    Update Transaction.cs
    add property:  TransactionDate

--- abc-bank/Transaction.cs
+++ abc-bank/Transaction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,12 @@ namespace abc_bank
 
         private DateTime transactionDate;
 
+        public DateTime  TransactionDate
+        {
+            get{return transactionDate;}
+        }
+       
+
         public Transaction(double amount) 
         {
             this.amount = amount;
@@ -19,3 +25,4 @@ namespace abc_bank
         }
     }
 }
+
